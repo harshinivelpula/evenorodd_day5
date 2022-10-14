8728cbcfacf9f51d1a4247505e58f21e53cbee49
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-14 20:57:23 +0530

    evenodd

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+
+
+namespace evenodd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int n;
+
+           
+            Console.WriteLine("Enter the number = ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            if (n % 2 == 0)
+                Console.WriteLine(n + " is even");
+            else
+                Console.WriteLine(n + " is odd");
+        }
+    }
+}
+
+
+
+
+
+
+
